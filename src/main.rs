8f9cbade423704717d05b3a97487eaf81818a078
Isaac Turner <spacey_sooty@outlook.com>
2023-08-19 22:55:09 +0800

    setup some stuff for pi

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+
+use core::panic::PanicInfo;
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    loop {
+        on_update();
+    }
 }
+
+fn on_update(){}
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
